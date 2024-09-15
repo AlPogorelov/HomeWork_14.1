bec31269fdde172f44161bdfd1802670907540fe
Al Pogorelov <eczemplrez@gmail.com>
2024-09-15 14:23:06 +0600

    Home work done

--- src/product.py
+++ src/product.py
@@ -9,5 +9,3 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
-
-    
--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,25 @@
 import json
 import os.path
+
 from src.category import Category
 from src.product import Product
 
 
 def open_json(file_name):
     full_file_name = os.path.abspath(file_name)
-    with open(full_file_name, 'r', encoding='UTF-8') as file:
+    with open(full_file_name, "r", encoding="UTF-8") as file:
         data = json.load(file)
         return data
+
 
 def create_from_json(data):
     categorys = []
     for category in data:
         products = []
-        for product in category['products']:
+        for product in category["products"]:
             products.append(Product(**product))
 
-        category['products'] = products
+        category["products"] = products
 
         categorys.append(Category(**category))
     return categorys
-
-if __name__ == '__main__':
-    data = open_json('../data/products.json')
-    print(create_from_json(data))
