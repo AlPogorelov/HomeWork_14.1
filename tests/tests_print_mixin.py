b6f71c2a67e92782ec31871e88e660926700dce9
Al Pogorelov <eczemplrez@gmail.com>
2024-10-04 17:56:36 +0600

    Home work 16.2 done

--- tests/tests_print_mixin.py
+++ tests/tests_print_mixin.py
@@ -1,7 +1,12 @@
-from src.product import Product
+from src.product import Product, Smartphone
 
 
 def test_print_mixin(capsys):
     Product("Samsung", "WIFI", 180000.0, 2)
     massage = capsys.readouterr()
     assert massage.out.strip() == 'Product(Samsung, WIFI, 180000.0, 2)'
+
+    Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 200.0, 2, 95.5,
+               "S23 Ultra", 256, "Серый")
+    massage = capsys.readouterr()
+    assert massage.out.strip() == 'Smartphone(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 200.0, 2)'
